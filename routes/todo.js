4b12fdd0a3e0cb1d2573ac2aff9c145f48518fe8
TakashiMotoki <t.motoki01@gmail.com>
2019-02-06 02:52:52 +0900

    mongodbに接続する処理のプロトタイプ処理を作成
    ＜プロトタイプ処理＞
    - mongodbから全件取得
    - それをそのままhttpリクエストに返す
    
    ＜対応予定＞
    - 接続処理の処理訳
    - レスポンスデータの加工

--- routes/todo.js
+++ routes/todo.js
@@ -1,9 +1,47 @@
+"use strict";
+
 const express = require('express');
 const router = express.Router();
+const log4js = require('log4js');
+log4js.configure('./log4js.config.json');
+
+const contentType = 'application/json; Accept-Charset=utf-8';
+
+// MongoDB関連
+const MongoClient = require("mongodb").MongoClient;
+const MongoUrl = "mongodb://localhost:27017";
+
+//logger
+const systemLogger = log4js.getLogger();
 
 // todo一覧取得
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+  let param = {};
+  MongoClient.connect(MongoUrl, {useNewUrlParser: true}, (error, client) => {
+
+      // コレクションの取得
+      const collection = client.db("todo").collection("list");
+  
+      // コレクションに含まれるドキュメントをすべて取得
+      collection.find({}).toArray((error, items) => {
+
+        param = items
+        res.header('Content-Type', contentType)
+        res.send(param);
+        
+        console.log("error:"+error);
+        console.log(items);
+        for(let item of items){
+          console.log(item.priority);
+          console.log(item.subject);
+          console.log(item.detail);
+        }
+        client.close();
+      });
+  
+  });
+
 });
 
 // タイトル＋ID一覧取得

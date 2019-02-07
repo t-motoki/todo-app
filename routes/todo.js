8d790fd859d2fb2f6c591bb7e9e038d8e204f908
TakashiMotoki <t.motoki01@gmail.com>
2019-02-07 23:47:28 +0900

    todo一覧検索を実装

--- routes/todo.js
+++ routes/todo.js
@@ -2,31 +2,60 @@
 
 const express = require('express');
 const router = express.Router();
-const log4js = require('log4js');
-log4js.configure('./log4js.config.json');
-
-const contentType = 'application/json; Accept-Charset=utf-8';
-
-// db model
-const item = require('../model');
 
 //logger
+const log4js = require('log4js');
+log4js.configure('./log4js.config.json');
 const systemLogger = log4js.getLogger();
+
+// データベースモデル
+const item = require('../model');
+
+// レスポンス情報
+const contentType = 'application/json; Accept-Charset=utf-8';
+const result_template = {
+  result:0,
+  message:""
+}
+
+// 正規表現で問題になる可能性がある文字列をエスケープ処理を実装
+RegExp.escape = s => s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+
 
 // todo一覧取得
 router.get('/', function(req, res, next) {
 
-  let param = {};
-  item.find({}, function(err, docs) {
-    if (err) console.error(err.errmsg)
-    param = docs;
-    console.log(param);
-    for(let doc of param){
-      console.log(doc.subject);
-      console.log(doc.detail);
-    }
+  // クエリの生成(どちらも指定されるとANDとなる)
+  let query = {}; // 全検索
+  let querylog = {};  // ログに正規表現をそのまま出力できないので別途用意
+  if("subject" in req.query){
+    const subject = RegExp.escape(req.query.subject);
+    query["subject"] = new RegExp(subject);
+    querylog["subject"] = `\/${subject}\/`;
+  }
+  if("detail" in req.query){
+    const detail = RegExp.escape(req.query.detail);
+    query["detail"] = new RegExp(detail);
+    querylog["detail"] = `\/${detail}\/`;
+  }
+  systemLogger.debug(`query:${JSON.stringify(querylog)}`);
+
+  // todo一覧を取得(IDは取得しない)
+  let result = JSON.parse(JSON.stringify(result_template));
+  systemLogger.debug(`result:${JSON.stringify(result)}`);
+  item.find(query, { _id:0 }, (err, docs) => {
     res.header('Content-Type', contentType)
-    res.send(param);
+    if (err){
+      result["result"] = 500;
+      result["message"] = err.errmsg;
+      systemLogger.error(err.errmsg);
+    }else{
+      // 正しく取得できた場合に格納
+      result["data"] = JSON.parse(JSON.stringify(docs));
+    }
+    
+    // 結果を返却
+    res.send(result);
   });
 });
 

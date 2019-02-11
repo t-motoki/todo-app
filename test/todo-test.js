bd22b07ac4fc9956a4f6ec65b37c55ebae7d3cd8
Takashi Motoki <t.motoki01@gmail.com>
2019-02-11 20:42:41 +0900

    サポートAPIのテストを追加

--- test/todo-test.js
+++ test/todo-test.js
@@ -1,10 +1,59 @@
-describe('ひな型', () => {
-  it('テスト内容', done => {
-    if('iiii' === 'iiii') {
-      done();
-    }
-    else {
-      done('失敗');
-    }
+const expect = require('expect');
+const app = require('../app');
+const supertest = require('supertest').agent(app.listen());
+
+const errorlist = require('../routes/apilist');
+const apilist = require('../routes/apilist');
+
+const checResponse = {
+  "error_no":{
+    result:0,
+    message:""
+  }
+};
+
+describe(`サポートAPIのチェック`, () => {
+  it('デモ用ページ読み込み', (done) => {
+    supertest.get('/')
+      .expect(200)
+      .end((error, response) => {
+        if(error) {
+          return done(error);
+        }
+        done();
+      });
+  });
+  it('API一覧取得', (done) => {
+    supertest.get('/api')
+      .expect(200)
+      .end((error, response) => {
+        if(error) {
+          return done(error);
+        }
+        expect(response.body).toHaveProperty("list");
+        done();
+      });
+  });
+  it('エラー一覧取得', (done) => {
+    supertest.get('/api/error')
+      .expect(200)
+      .end((error, response) => {
+        if(error) {
+          return done(error);
+        }
+        expect(response.body).toHaveProperty("list");
+        done();
+      });
+  });
+  it('API仕様書へ転送', (done) => {
+    supertest.get('/api/spec')
+      .expect(302)
+      .end((error, response) => {
+        if(error) {
+          return done(error);
+        }
+        done();
+      });
   });
 });
+

b7c2f5d42ea0afdd21c2465ce5a991b77a6bd54b
Takashi Motoki <t.motoki01@gmail.com>
2019-02-15 23:07:42 +0900

    仕様変更を実装

--- routes/todo.js
+++ routes/todo.js
@@ -33,16 +33,37 @@ const createConditionalQuery = reqQuery => {
   // クエリの生成(複数指定されるとANDとなる)
   let query = {}; // 全検索
   let querylog = {};  // ログに正規表現をそのまま出力できないので別途用意
+
+  // タイトル
   if("subject" in reqQuery){
-    const subject = RegExp.escape(reqQuery.subject);
-    query["subject"] = new RegExp(subject);
-    querylog["subject"] = `/${subject}/`;
+    // 完全一致検索かどうか
+    let exact = false;
+    if("exact" in reqQuery){
+      if(reqQuery["exact"]==="true"){
+        exact = true;
+      }
+    }
+
+    if(exact){
+      // 完全一致
+      query["subject"] = reqQuery.subject;
+      querylog["subject"] = reqQuery.subject;
+    }else{
+      // 部分一致
+      const subject = RegExp.escape(reqQuery.subject);
+      query["subject"] = new RegExp(subject);
+      querylog["subject"] = `/${subject}/`;
+    }
   }
+
+  // タイトル
   if("detail" in reqQuery){
+    // 部分一致
     const detail = RegExp.escape(reqQuery.detail);
     query["detail"] = new RegExp(detail);
     querylog["detail"] = `/${detail}/`;
   }
+
   if("done" in reqQuery){
     if(reqQuery.done==="true" || reqQuery.done==="false"){
       // 正しい指定時のみ条件に入れる
@@ -117,37 +138,6 @@ router.get('/subjects', (req, res) =>  {
   });
 });
 
-// todoを取得(タイトル指定して1件取得)
-router.get('/:subject', (req, res) => {
-
-  // クエリの生成
-  const query = {subject:req.params.subject};
-  systemLogger.debug(`query:${JSON.stringify(query)}`);
-
-  // todoを取得(IDとバージョンは取得しない)
-  let result = JSON.parse(JSON.stringify(result_template));
-  item.findOne(query, { _id:0, __v:0 }, (err, docs) => {
-    res.header('Content-Type', contentType);
-    if (err){
-      result["result"] = EL.E_RUN_DATABASE.num;
-      result["message"] = "データベース実行時にエラーが発生しました。詳細 => ";
-      result["message"] += JSON.stringify(err);
-      systemLogger.error(`result:${result["result"]}, message:${result["message"].replace(/\r?\n/g,'')}`);
-    }else{
-      if(docs){
-        // 正しく取得できた場合に格納
-        result["data"] = JSON.parse(JSON.stringify(docs));
-      }else{
-        // ない場合は空のオブジェクトを返す
-        result["data"] = {};
-      }
-    }
-    
-    // 結果を返却
-    res.send(result);
-  });
-
-});
 
 // todo追加
 router.post('/', (req, res, next) => {
@@ -171,14 +161,6 @@ router.post('/', (req, res, next) => {
     // エラー処理
     next();
   }else{
-
-    if(req.body.subject==="subjects"){
-      res.header('Content-Type', contentType);
-      return res.send({
-        result:EL.E_PRM_PROHIBITED.num,
-        message:"[subjects]はAPI名の為、登録できません。"
-      });
-    }  
 
     // クエリの生成
     const query = {subject:req.body.subject};
@@ -243,35 +225,25 @@ router.post('/', (req, res, next) => {
 
 });
 
-// todo更新で引数がない場合にエラーを返す
-router.put('/', (req, res) => {
-
-  // 引数が足りていないためエラー
-  let result = {
-    result: EL.E_PRM_NOTENOUGH.num,
-    message: `更新対象となる元のsubjectが指定されていません。`
-  };
-  systemLogger.error(`result:${result["result"]}, message:${result["message"].replace(/\r?\n/g,'')}`);
-
-  // エラーを返却
-  res.header('Content-Type', contentType);
-  res.send(result);
-});
-
 // 指定したtodo更新
-router.put('/:subject', (req, res, next) => {
+router.put('/', (req, res, next) => {
+
+  // 更新対象が指定されてない場合はエラーとする
+  if(!("subject" in req.query)){
+    const result = {
+      result: EL.E_PRM_NOTENOUGH.num,
+      message: `更新対象となる元のsubjectが指定されていません。`
+    };
+    systemLogger.error(`result:${result["result"]}, message:${result["message"]}`);
+    // エラーを返却
+    res.header('Content-Type', contentType);
+    return res.send(result);
+  }
 
   // 登録データの作成
   let register = {};
   if("subject" in req.body){
     register["subject"] = req.body["subject"];
-    if(register["subject"]==="subjects"){
-      res.header('Content-Type', contentType);
-      return res.send({
-        result:EL.E_PRM_PROHIBITED.num,
-        message:"[subjects]はAPI名の為、登録できません。"
-      });
-    }  
   }
   if("detail" in req.body){
     register["detail"] = req.body["detail"];
@@ -285,7 +257,7 @@ router.put('/:subject', (req, res, next) => {
     next();
   }else{
     // クエリの生成
-    const query = {subject:req.params.subject};
+    const query = {subject:req.query.subject};
     systemLogger.debug(`query:${JSON.stringify(query)}`);
 
     // 登録データをログ出力
@@ -330,7 +302,7 @@ router.put('/:subject', (req, res, next) => {
         // DBに存在していないタイトルが更新対象に指定された
         if(docs.n === 0){
           result["result"] = EL.E_SRH_NOTTERGET.num;
-          result["message"] = `更新対象subject:"${req.params.subject}"はデータベースに存在しません`;
+          result["message"] = `更新対象subject:"${req.query.subject}"はデータベースに存在しません`;
           systemLogger.error(`result:${result["result"]} , message:${result["message"].replace(/\r?\n/g,'')}`);
         }
       }
@@ -340,9 +312,9 @@ router.put('/:subject', (req, res, next) => {
 }, (req, res) => {
 
   // 引数が足りていないためエラー
-  let result = {
+  const result = {
     result: EL.E_PRM_NOTENOUGH.num,
-    message: `有効な更新データが1つも指定されていません。`
+    message: `登録用データ何も有効な更新データが1つも指定されていません。`
   };
   systemLogger.error(`result:${result["result"]}, message:${result["message"]}`);
 
